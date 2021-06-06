89d9ee9c391b47046c7380b28d89a7cbf58cc8a2
wenlin <569958268@qq.com>
2021-06-06 10:28:08 +0800

    初始化vite

---
+++ src/components/test.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  // 类型推断启用
+  render() {
+    return <div>3dagsd</div>
+  }
+})
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
+
+import test from './components/test';
+
+new Vue(test).$mount('#app')
